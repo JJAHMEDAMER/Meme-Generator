e35851ae8e98bca298f155cd290e699c95ee3a5a
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-04 17:45:12 +0200

    set state random meme

--- src/comp/form.js
+++ src/comp/form.js
@@ -1,11 +1,26 @@
 import React from "react";
+import data from "../memeData";
 
 function Form() {
+  // declaring State variable
+  let src = data["data"]["memes"][Math.floor(Math.random() * 100)]["url"];
+  
+  // Getting state function
+  const [_notNeeded, setSrc] = React.useState(src);
+
+  // Changing State
+  function randomMeme() {
+    src = data["data"]["memes"][Math.floor(Math.random() * 100)]["url"];
+    setSrc(src);
+  }
   return (
     <div className="form">
       <input className="form--input" placeholder="Top Text" />
       <input className="form--input" placeholder="Bottom Text" />
-      <button className="form--button">Get A Meme</button>
+      <button onClick={randomMeme} className="form--button">
+        Get A Meme
+      </button>
+      <img className="meme--img" src={src} alt="meme" />
     </div>
   );
 }
