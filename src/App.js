d64e362d97d2184f5be533558d06848435b84756
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-04 16:30:42 +0200

    input form

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import React from "react";
 // Comp
 import NavBar from "./comp/NavBar";
 
+import Form from "./comp/form";
+
 function App() {
   return (
     <div>
       <NavBar/>
+      <Form/>
     </div>
   );
 }
---
+++ src/comp/form.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Form() {
+  return (
+    <div className="form">
+      <input className="form--input" placeholder="Top Text" />
+      <input className="form--input" placeholder="Bottom Text" />
+      <button className="form--button">Get A Meme</button>
+    </div>
+  );
+}
+
+export default Form;
