d87178f5b12ac04fb607498e512f46f74e32930e
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-11-05 07:29:52 +0200

    meme object

--- src/comp/form.js
+++ src/comp/form.js
@@ -2,9 +2,28 @@ import React from "react";
 import data from "../memeData";
 
 function Form() {
+
+  const memeObj = {
+    img: data["data"]["memes"][0]["url"],
+    topText: "Hello",
+    bottom: "World",
+  };
+
+  const [meme, setMyMeme] = React.useState(memeObj);
+
+  function randomMeme() {
+    setMyMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        img: data["data"]["memes"][Math.floor(Math.random() * 100)]["url"],
+      };
+    });
+  }
+
+  /*
   // declaring State variable
   let src = data["data"]["memes"][Math.floor(Math.random() * 100)]["url"];
-  
+
   // Getting state function
   const [_notNeeded, setSrc] = React.useState(src);
 
@@ -13,6 +32,7 @@ function Form() {
     src = data["data"]["memes"][Math.floor(Math.random() * 100)]["url"];
     setSrc(src);
   }
+*/
   return (
     <div className="form">
       <input className="form--input" placeholder="Top Text" />
@@ -20,7 +40,7 @@ function Form() {
       <button onClick={randomMeme} className="form--button">
         Get A Meme
       </button>
-      <img className="meme--img" src={src} alt="meme" />
+      <img className="meme--img" src={meme.img} alt="meme" />
     </div>
   );
 }
